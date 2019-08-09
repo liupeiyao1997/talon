3aed5591b4c17225b1f066bdd87de944ec6f4a65
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 14:06:02 +0800

    son of beach

---
+++ GetPrize2.java
@@ -0,0 +1,10 @@
+public class GetPrize2{
+	public static void main(String[]args){
+		int score1=100;
+		int score2=72;
+		if((score1>98&&score2>80)||(score1==100&&score2>70)){
+			System.out.println("老师说:不错，奖励一个MP4!");
+		}
+			
+	}
+}
