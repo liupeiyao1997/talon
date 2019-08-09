1a520f2298b9140c44a23a1e9c03ea0dbf5291bb
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 11:34:49 +0800

    fucking gog

---
+++ DXC.java
@@ -0,0 +1,11 @@
+public class DXC{
+	public static void main (String[]args){
+		int fenshu=100;
+		if(fenshu>98){
+			System.out.println("老师给8个MP4\t6464644");
+			
+		}else{
+			System.out.println("进行编码\n434343");
+			System.out.println("一直学");
+		}
+	}
