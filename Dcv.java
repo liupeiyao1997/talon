bd5c501c20da3ed93b7313793d40150b81d2af77
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 13:16:09 +0800

    test

--- Dcv.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Dcv{
-	public static void main(String[]args){
-		Scanner input=new Scanner(System.in);
-		System.out.println("shi:");
-		int ssb= input.nextInt();
-   		
-		}
-}
