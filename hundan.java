94c3500458e55f72c7a70dbcd11e23d5fa9e9161
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 14:16:40 +0800

    caosigou

---
+++ hundan.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class hundan{
+	public static void main(String[]args){
+		Scanner gg=new Scanner(System.in);
+		System.out.println("sdfsdf:");
+		int num=gg.nextInt();
+	}
+}
