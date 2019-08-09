7f06f37168597e9a0cc5996dced7699d87ee969e
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 15:23:58 +0800

    bisitonight

--- GetPrize2.java
+++ GetPrize2.java
@@ -3,7 +3,7 @@ public class GetPrize2{
 		int score1=100;
 		int score2=72;
 		if((score1>98&&score2>80)||(score1==100&&score2>70)){
-			System.out.println("老师说:不错，奖励一个MP4!");
+			System.out.println("你的亲妈今晚必死，不死不是亲妈!");
 		}
 			
 	}
