815f4a2b648b01c001bb5da6110c886dd751ef4a
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 13:23:50 +0800

    asdasd456

--- DDSS.java
+++ DDSS.java
@@ -8,7 +8,7 @@ public class DDSS{
 			case 2:System.out.println("奖励电脑一台");
 			break;
 			case 3:
-			System.out.println("奖励一桶油漆喝");
+			System.out.println("奖励硬盘一个");
 			break;
 			default:
 			System.out.println("没有任何奖励");
