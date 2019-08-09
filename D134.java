d6140f21e6e2690702c4fd5b76e7c14403c889bd
liupeiyao1997 <liupeiyao1997@163.com>
2019-08-09 12:06:22 +0800

    son of bitch

---
+++ D134.java
@@ -0,0 +1,5 @@
+public class D134{
+	public static void main(String[]args){
+		System.out.println("1fdddff");
+	}
+}
---
+++ Dcv.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Dcv{
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		System.out.println("shi:");
+		int ssb= input.nextInt();
+   		
+		}
+}
